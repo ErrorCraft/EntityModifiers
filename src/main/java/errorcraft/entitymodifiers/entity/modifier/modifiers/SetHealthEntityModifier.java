445f4247afa227041e948efd822c89f2e332807e
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2021-08-13 16:34:14 +0200

    Use number providers where numbers were previously used in entity modifiers

--- src/main/java/errorcraft/entitymodifiers/entity/modifier/modifiers/SetHealthEntityModifier.java
+++ src/main/java/errorcraft/entitymodifiers/entity/modifier/modifiers/SetHealthEntityModifier.java
@@ -9,14 +9,15 @@ import errorcraft.entitymodifiers.entity.modifier.EntityModifierTypes;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.loot.context.LootContext;
+import net.minecraft.loot.provider.number.LootNumberProvider;
 import net.minecraft.util.JsonHelper;
 
 public class SetHealthEntityModifier implements EntityModifier {
-	private final float health;
+	private final LootNumberProvider healthProvider;
 	private final boolean add;
 
-	public SetHealthEntityModifier(float health, boolean add) {
-		this.health = health;
+	public SetHealthEntityModifier(LootNumberProvider healthProvider, boolean add) {
+		this.healthProvider = healthProvider;
 		this.add = add;
 	}
 
@@ -28,28 +29,28 @@ public class SetHealthEntityModifier implements EntityModifier {
 	@Override
 	public Entity apply(Entity entity, LootContext lootContext) {
 		if (entity instanceof LivingEntity livingEntity) {
-			setHealth(livingEntity);
+			setHealth(livingEntity, lootContext);
 		}
 		return entity;
 	}
 
-	private void setHealth(LivingEntity livingEntity) {
+	private void setHealth(LivingEntity livingEntity, LootContext lootContext) {
 		float newHealth = this.add ? livingEntity.getHealth() : 0.0f;
-		livingEntity.setHealth(newHealth + this.health);
+		livingEntity.setHealth(newHealth + this.healthProvider.nextFloat(lootContext));
 	}
 
 	public static class Serialiser implements EntityModifier.Serialiser<SetHealthEntityModifier> {
 		@Override
 		public void toJson(JsonObject json, SetHealthEntityModifier object, JsonSerializationContext context) {
-			json.addProperty("health", object.health);
+			json.add("health", context.serialize(object.healthProvider));
 			json.addProperty("add", object.add);
 		}
 
 		@Override
 		public SetHealthEntityModifier fromJson(JsonObject json, JsonDeserializationContext context) {
-			float health = JsonHelper.getFloat(json, "health");
+			LootNumberProvider healthProvider = JsonHelper.deserialize(json, "health", context, LootNumberProvider.class);
 			boolean add = JsonHelper.getBoolean(json, "add", false);
-			return new SetHealthEntityModifier(health, add);
+			return new SetHealthEntityModifier(healthProvider, add);
 		}
 	}
 }
--- src/main/java/errorcraft/entitymodifiers/entity/modifier/modifiers/SetHungerEntityModifier.java
+++ src/main/java/errorcraft/entitymodifiers/entity/modifier/modifiers/SetHungerEntityModifier.java
@@ -10,15 +10,16 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.HungerManager;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.loot.context.LootContext;
+import net.minecraft.loot.provider.number.LootNumberProvider;
 import net.minecraft.util.JsonHelper;
 import net.minecraft.util.math.MathHelper;
 
 public class SetHungerEntityModifier implements EntityModifier {
-	private final int hunger;
+	private final LootNumberProvider hungerProvider;
 	private final boolean add;
 
-	public SetHungerEntityModifier(int hunger, boolean add) {
-		this.hunger = hunger;
+	public SetHungerEntityModifier(LootNumberProvider hungerProvider, boolean add) {
+		this.hungerProvider = hungerProvider;
 		this.add = add;
 	}
 
@@ -30,29 +31,29 @@ public class SetHungerEntityModifier implements EntityModifier {
 	@Override
 	public Entity apply(Entity entity, LootContext lootContext) {
 		if (entity instanceof PlayerEntity player) {
-			setHunger(player);
+			setHunger(player, lootContext);
 		}
 		return entity;
 	}
 
-	private void setHunger(PlayerEntity player) {
+	private void setHunger(PlayerEntity player, LootContext lootContext) {
 		HungerManager hungerManager = player.getHungerManager();
 		int newHunger = this.add ? hungerManager.getFoodLevel() : 0;
-		hungerManager.setFoodLevel(MathHelper.clamp(newHunger + this.hunger, 0, 20));
+		hungerManager.setFoodLevel(MathHelper.clamp(newHunger + this.hungerProvider.nextInt(lootContext), 0, 20));
 	}
 
 	public static class Serialiser implements EntityModifier.Serialiser<SetHungerEntityModifier> {
 		@Override
 		public void toJson(JsonObject json, SetHungerEntityModifier object, JsonSerializationContext context) {
-			json.addProperty("hunger", object.hunger);
+			json.add("hunger", context.serialize(object.hungerProvider));
 			json.addProperty("add", object.add);
 		}
 
 		@Override
 		public SetHungerEntityModifier fromJson(JsonObject json, JsonDeserializationContext context) {
-			int hunger = JsonHelper.getInt(json, "hunger");
+			LootNumberProvider hungerProvider = JsonHelper.deserialize(json, "hunger", context, LootNumberProvider.class);
 			boolean add = JsonHelper.getBoolean(json, "add", false);
-			return new SetHungerEntityModifier(hunger, add);
+			return new SetHungerEntityModifier(hungerProvider, add);
 		}
 	}
 }
