ff721165205aea0d63ab66582af2cec4a7c4a403
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2021-08-13 16:32:08 +0200

    Allow number providers to be used in entity modifiers

--- src/main/java/errorcraft/entitymodifiers/entity/modifier/Deserialisers.java
+++ src/main/java/errorcraft/entitymodifiers/entity/modifier/Deserialisers.java
@@ -1,11 +1,18 @@
 package errorcraft.entitymodifiers.entity.modifier;
 
 import com.google.gson.GsonBuilder;
+import net.minecraft.loot.provider.number.LootNumberProvider;
+import net.minecraft.loot.provider.number.LootNumberProviderTypes;
+import net.minecraft.loot.provider.score.LootScoreProvider;
+import net.minecraft.loot.provider.score.LootScoreProviderTypes;
 
 class Deserialisers {
 	private Deserialisers() {}
 
 	public static GsonBuilder createEntityModifierSerialiser() {
-		return new GsonBuilder().registerTypeHierarchyAdapter(EntityModifier.class, EntityModifierTypes.createGsonAdapter());
+		return new GsonBuilder()
+				.registerTypeHierarchyAdapter(EntityModifier.class, EntityModifierTypes.createGsonAdapter())
+				.registerTypeHierarchyAdapter(LootNumberProvider.class, LootNumberProviderTypes.createGsonSerializer())
+				.registerTypeHierarchyAdapter(LootScoreProvider.class, LootScoreProviderTypes.createGsonSerializer());
 	}
 }
