c01a1ae776b5f44cd17b626014c88f36b2a3b0e9
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2021-10-29 14:41:22 +0200

    Fix score number providers not allowing entity targets

--- src/main/java/errorcraft/entitymodifiers/entity/modifier/Deserialisers.java
+++ src/main/java/errorcraft/entitymodifiers/entity/modifier/Deserialisers.java
@@ -1,6 +1,7 @@
 package errorcraft.entitymodifiers.entity.modifier;
 
 import com.google.gson.GsonBuilder;
+import net.minecraft.loot.context.LootContext;
 import net.minecraft.loot.provider.number.LootNumberProvider;
 import net.minecraft.loot.provider.number.LootNumberProviderTypes;
 import net.minecraft.loot.provider.score.LootScoreProvider;
@@ -13,6 +14,7 @@ class Deserialisers {
 		return new GsonBuilder()
 				.registerTypeHierarchyAdapter(EntityModifier.class, EntityModifierTypes.createGsonAdapter())
 				.registerTypeHierarchyAdapter(LootNumberProvider.class, LootNumberProviderTypes.createGsonSerializer())
-				.registerTypeHierarchyAdapter(LootScoreProvider.class, LootScoreProviderTypes.createGsonSerializer());
+				.registerTypeHierarchyAdapter(LootScoreProvider.class, LootScoreProviderTypes.createGsonSerializer())
+				.registerTypeHierarchyAdapter(LootContext.EntityTarget.class, new LootContext.EntityTarget.Serializer());
 	}
 }
